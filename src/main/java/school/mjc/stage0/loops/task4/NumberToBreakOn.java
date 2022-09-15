e379ce1c746d4f33f43f0093583f486508546546
cyberdefender001 <101460837+cyberdefender001@users.noreply.github.com>
2022-09-15 16:31:15 +0500

    Update NumberToBreakOn.java

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -2,6 +2,17 @@ package school.mjc.stage0.loops.task4;
 
 public class NumberToBreakOn {
     public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
-
+        if (i < toBreakWith) {
+            System.out.println("iterating till the end");
+            
+            for (int i = 0; i < numberToGoUntil; i++) {
+                System.out.println(i);
+            }
+        } else {
+            for (int i = 0; i < toBreakWith; i++) {
+                System.out.println(i);
+            }
+        }
+        
     }
 }
